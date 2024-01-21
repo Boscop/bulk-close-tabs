28a994837a7102069bce44763df604180012d3f6
Boscop <Boscop@users.noreply.github.com>
2024-01-21 05:02:32 +0200

    Add support for closing additional tabs & windows

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 function announce_close() {
-	console.log("closing all youtube tabs")
 	chrome.tabs.query({}, function (tabs) {
+		console.log("bulk closing tabs");
 		var closeable = [];
 		for (var i = 0; i < tabs.length; ++i) {
 			// console.log(tabs[i]);
@@ -9,6 +9,9 @@ function announce_close() {
 				"youtu.be/",
 				"google.com/search?",
 				"duckduckgo.com/?q=",
+				"https://twitter.com/",
+				"spotify.com/",
+				"soundcloud.com/",
 			]
 			for (const pattern of patterns) {
 				if (tabs[i].url.includes(pattern)) {
@@ -21,6 +24,39 @@ function announce_close() {
 		chrome.tabs.remove(closeable);
 		console.log("tabs closed");
 	});
+	/* chrome.windows.getAll({ populate: true }, function (windows) {
+		console.log("bulk closing windows", windows.length);
+		var maxWidth = 0;
+		var maxHeight = 0;
+		for (const window of windows) {
+			if (window.width > maxWidth) {
+				maxWidth = window.width;
+			}
+			if (window.height > maxHeight) {
+				maxHeight = window.height;
+			}
+		}
+		console.log("max window size:", maxWidth, maxHeight);
+
+		const closeable = [];
+		for (const window of windows) {
+			if (window.type === "normal") {
+				if (window.width < maxWidth || window.height < maxHeight) {
+					const tab = window.tabs[0];
+					console.log("keeping window:", window.type, window.id, tab.title, tab.url);
+				} else {
+					closeable.push(window.id);
+				}
+			}
+		}
+		console.log("closing " + closeable.length + " windows");
+		for (const id of closeable) {
+			chrome.windows.remove(id, function () {
+				console.log("window closed:", id);
+			});
+		}
+		console.log("windows closed");
+	}); */
 }
 chrome.browserAction.onClicked.addListener(function (tab) {
 	announce_close();
